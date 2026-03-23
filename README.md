bf0ee387d41135026c02f9de13cb1d00a0ace10b
strek-o <strek.oliwier@gmail.com>
2026-03-23 17:39:01 +0100

    Add zadanie02-3.5

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Punkty 3.0-4.5 powinny mieć osobny obraz Dockerowy.
 Należy stworzyć aplikację na frameworku Play lub Scalatra.
 
 - [x] **3.0** Należy stworzyć kontroler do Produktów
-- [ ] **3.5** Do kontrolera należy stworzyć endpointy zgodnie z CRUD - dane pobierane z listy
+- [x] **3.5** Do kontrolera należy stworzyć endpointy zgodnie z CRUD - dane pobierane z listy
 - [ ] **4.0** Należy stworzyć kontrolery do Kategorii oraz Koszyka + endpointy zgodnie z CRUD
 - [ ] **4.5** Należy aplikację uruchomić na dockerze (stworzyć obraz) oraz dodać skrypt uruchamiający aplikację via ngrok
 - [ ] **5.0** Należy dodać konfigurację CORS dla dwóch hostów dla metod CRUD
--- zadanie02/app/controllers/ProductController.scala
+++ zadanie02/app/controllers/ProductController.scala
@@ -3,11 +3,12 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import models.Product
+import scala.collection.mutable.ListBuffer
 
 @Singleton
 class ProductController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  val productList = List(
+  val productList = ListBuffer(
     Product(1, "Example01", 1.11),
     Product(2, "Example02", 2.22),
     Product(3, "Example03", 3.33)
@@ -16,5 +17,37 @@ class ProductController @Inject()(val controllerComponents: ControllerComponents
   def getAll() = Action { implicit request: Request[AnyContent] =>
     val response = productList.map(p => s"(${p.id}, ${p.name}, ${p.price})").mkString("\n")
     Ok(response)
+  }
+
+  def get(id: Int) = Action {
+    productList.find(p => p.id == id) match {
+      case Some(p) => Ok(s"(${p.id}, ${p.name}, ${p.price})")
+      case None => NotFound(s"Not Found")
+    }
+  }
+
+  def add(id: Int, name: String, price: Double) = Action {
+    productList += Product(id, name, price)
+    Ok(s"OK")
+  }
+
+  def update(id: Int, newName: String, newPrice: Double) = Action {
+    val index = productList.indexWhere(p => p.id == id)
+    if (index >= 0) {
+      productList(index) = Product(id, newName, newPrice)
+      Ok(s"OK")
+    } else {
+      NotFound(s"Not Found")
+    }
+  }
+
+  def delete(id: Int) = Action {
+    val index = productList.indexWhere(p => p.id == id)
+    if (index >= 0) {
+      productList.remove(index)
+      Ok(s"OK")
+    } else {
+      NotFound(s"Not Found")
+    }
   }
 }
